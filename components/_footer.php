674d7decd0b905354a3fd9b6e555919062304f45
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-02-11 03:19:58 +0530

    Adding Header , Footer, Card with unsplash photo api  and Creating A database table and connection

---
+++ components/_footer.php
@@ -0,0 +1,5 @@
+<div class="contaner-fluid bg-dark text-light">
+    <p class="text-center mb-0">
+        Copyright 2024 | All right reserved -Mayank Rajput...
+    </p>
+</div>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to iTech Forum</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include 'components/_header.php'; ?>
+    <?php include 'components/_dbconnect.php'; ?>
+    <div class="container">
+        <h2 class="text-center my-3"> iTech- Categories</h2>
+        <div class="row">
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <?php include 'components/_footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
