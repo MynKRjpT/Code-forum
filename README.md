8fb7132698748c59a734af5deafcea774f1592e3
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-02-12 20:45:15 +0530

    Adding Cards

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Code-forum
  This is a coding forum in which we can discuss about any question or doubt in any technology.
+ 
+ 
+ 1- "Adding Header , Footer, Card with unsplash photo api  and Creating A database table and connection "
--- index.php
+++ index.php
@@ -22,6 +22,56 @@
                 </div>
                 </div>
             </div>
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+                </div>
+            </div>
         </div>
     </div>
 
