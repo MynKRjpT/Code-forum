2db33773aea38ee4f8b72821288f32376c7e7937
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-02-13 19:03:12 +0530

    Adding modal

--- index.php
+++ index.php
@@ -1,75 +1,109 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome to iTech Forum</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
     <?php include 'components/_header.php'; ?>
+    <?php include 'components/_login.php'; ?>
     <?php include 'components/_dbconnect.php'; ?>
     <div class="container">
+
         <h2 class="text-center my-3"> iTech- Categories</h2>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://source.unsplash.com/2100x700/?coding,code" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://source.unsplash.com/2100x700/?coding,technology" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://source.unsplash.com/2100x700/?coding,thoughts" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         <div class="row">
             <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
-                </div>
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
-                </div>
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
-                </div>
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4">
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">View Threads</a>
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
                 </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?coding,java" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -78,6 +112,16 @@
 
 
     <?php include 'components/_footer.php'; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+        integrity="sha384-Nd/zR5ei9g/EWhZk+9t2yhJQnJlNm0DLeL8lD3rN1op09vhE8HzNcCA06MDm+J6j" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-5KQSG5f/m6m6f5FkR8/RnFJXa+a5ORgeUpnXbYGh1iIc8hX8esuEhsusVehux2Fj" crossorigin="anonymous">
+    </script>
+
+</body>
+
 </html>
